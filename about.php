d3ead818ee92c908bebac229b3b1f0920d5cc448
Zoe Lord <zoelord@rocketmail.com>
2024-12-15 20:19:55 -0500

    polished about me page

--- about.php
+++ about.php
@@ -14,11 +14,26 @@
     <canvas id="orb-canvas"></canvas>
 
     <!-- About Section -->
-    <main>
-        <h1>About Me</h1>
-        <p>I swim.</p>
-    </main>
+    <main class="about-container">
+    <!-- Headshot Image -->
+    <div class="about-image">
+        <img src="headshot_image.jpg" alt="Zoé Victoria Lord">
+    </div>
 
+    <!-- Header Title -->
+    <div class="about-header">
+        <h1>About Me</h1>
+    </div>
+
+    <!-- Text Content -->
+    <div class="about-text">
+        <p>With a strong passion for interdisciplinary learning across STEM disciplines, Zoé is driven to gain a unique perspective for addressing the complexities inherent in science and engineering. Zoé’s fascination with neuroscience has led her to pursue further knowledge on the chemical and biological factors influencing the effects of spaceflight on the human body. She developed a research payload for a high-fidelity CPR manikin optimized for space environments which was successfully tested on parabolic flight. She is also involved in exploring the impact of lunar surface EVA conditions on astronaut health and operational proficiency. 
+        <p>Zoé holds an academic background in Behavioural Neuroscience (2019), Chemistry, and Biology (2022). She is currently pursuing her PhD at Queen’s University, where her research is titled “An Investigation of Cognitive Overload in Virtual Reality Learning Environments”. In 2023, Zoé joined the International Institute for Astronautical Sciences (IIAS) as a Scientist-Astronaut candidate, where she is concurrently studying Bioastronautics with a concentration in EVA Space Suit Evaluation. 
+        <p>The road to Zoé’s accomplishments has been an unconventional journey. She met many hardships that she had to overcome with perseverance and resilience in the pursuit of her goals. While some educators did not recognize her potential and encouraged her to pursue a different path, Zoé’s determination to build a career in STEM drove her to push beyond expectations.</p>
+    </div>
+</main>
+
+    <!-- Footer -->
     <?php include 'footer.html'; ?>
 </body>
 </html>

324b2c2354f92bf8e13a92c76f873e4723740179
Zoe Lord <zoelord@rocketmail.com>
2024-12-20 00:05:20 -0500

    added favicons

--- tech_skills.php
+++ tech_skills.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.png" type="image/png" sizes="16x16">
+    <link rel="icon" href="images/favicon.png" type="image/png" sizes="32x32">
+    <link rel="apple-touch-icon" href="images/favicon.png">
     <title>Skills - Zoé Victoria Lord</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
-
 </head>
 <body>
     <!-- Navigation -->
